fb56c97b0c78ef74093b3bef93a33f63bb530190
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-07-15 12:27:16 +0700

    Add Headers to LoginApiStep

--- src/test/java/steps/auth/API/LoginApiStep.java
+++ src/test/java/steps/auth/API/LoginApiStep.java
@@ -2,11 +2,16 @@ package steps.auth.API;
 
 import config.classes.MainProps;
 import io.qameta.allure.Step;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,24 +20,37 @@ import static io.restassured.RestAssured.given;
  */
 public class LoginApiStep {
     private String request = null;
+    private String token = null;
 
     @Step("Авторизация API")
-    public void authorizationAPI(String userName, String password) {
+    public String authorizationAPI(String userName, String password) {
         try {
             request = new String(Files.readAllBytes(Paths.get("src/test/resources/requests/login.json")),
                     StandardCharsets.UTF_8)
                     .replace("${password}", password)
                     .replace("${userName}", userName);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
 
-        given().when()
+        String response = given().when()
                 .header("Content-Type", "application/json;charset=utf-8")
                 .body(request)
-                .post(MainProps.environmentProps.apiUrl() +"/login")
+                .post(MainProps.environmentProps.apiUrl() + "/login")
                 .then()
                 .assertThat().statusCode(202)
                 .extract().body().asString();
+        JSONObject jsonObject = new JSONObject(response);
+        token = jsonObject.getString("access_token");
+        return token;
+    }
+
+    public Headers httpHeaderManager() {
+        Header contentType = new Header("Content-Type", "application/json;charset=utf-8");
+        Header authorization = new Header("Authorization", "Bearer " + new LoginApiStep().authorizationAPI("user@pflb.ru", "user"));
+        List<Header> headerList = new ArrayList<>();
+        headerList.add(contentType);
+        headerList.add(authorization);
+        return new Headers(headerList);
     }
 }
