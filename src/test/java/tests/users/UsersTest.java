ef3831a06c5e606f0abe6615d6939dc8a4a807ea
Trifonov Dmitriy <dmitriy@it-qa.ru>
2024-07-20 19:34:37 +0700

    UI Test createUser without Age

--- src/test/java/tests/users/UsersTest.java
+++ src/test/java/tests/users/UsersTest.java
@@ -1,26 +1,34 @@
 package tests.users;
 
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import steps.users.API.UsersApiStep;
 import steps.users.UI.UsersUIStep;
+import web.pages.UserPage;
 
 public class UsersTest {
     @Test
+    @Owner("Trifonov Dmitriy")
     @DisplayName("Создание пользователя, API")
     public void userCreateApi() {
         new UsersApiStep().createUserApi();
     }
 
     @Test
+    @Owner("Trifonov Dmitriy")
     @DisplayName("Создание и удаление пользователя, API")
     public void userDeleteApi() {
         new UsersApiStep().createUserApi().deleteUserApi();
     }
 
     @Test
+    @Owner("Trifonov Dmitriy")
     @DisplayName("Создание пользователя, UI (Негативный, без указания возраста)")
     public void userCreateUIWithoutAge() {
         UsersUIStep.createUserUI();
+        String actualText = new UserPage().getStatus().getText();
+        Assertions.assertEquals("Status: Invalid request data", actualText, "Ожидаемый текст, не соответсвует действительному!");
     }
 }
